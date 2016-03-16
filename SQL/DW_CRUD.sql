c41be897027e00285111a6ded4737af0c7773cb7
ivajo26 <ivajodimo26@gmail.com>
2016-03-15 20:30:47 -0500

    update Profile

--- SQL/DW_CRUD.sql
+++ SQL/DW_CRUD.sql
@@ -3,7 +3,7 @@
 -- http://www.phpmyadmin.net
 --
 -- Host: localhost
--- Generation Time: Mar 11, 2016 at 12:57 AM
+-- Generation Time: Mar 15, 2016 at 08:29 PM
 -- Server version: 5.5.47-0ubuntu0.14.04.1
 -- PHP Version: 5.5.9-1ubuntu4.14
 
@@ -31,7 +31,7 @@ CREATE TABLE IF NOT EXISTS `adviser` (
   `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=2 ;
 
 -- --------------------------------------------------------
 
@@ -43,10 +43,22 @@ CREATE TABLE IF NOT EXISTS `author` (
   `id` int(11) NOT NULL AUTO_INCREMENT,
   `first_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
   `last_name` varchar(50) COLLATE utf8_unicode_ci NOT NULL,
-  `id_project` int(11) NOT NULL,
+  `id_project` int(11) DEFAULT NULL,
   PRIMARY KEY (`id`),
   KEY `id_project` (`id_project`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=50 ;
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `investigation_line`
+--
+
+CREATE TABLE IF NOT EXISTS `investigation_line` (
+  `id` int(11) NOT NULL AUTO_INCREMENT,
+  `name` varchar(200) NOT NULL,
+  PRIMARY KEY (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=latin1 AUTO_INCREMENT=1 ;
 
 -- --------------------------------------------------------
 
@@ -60,10 +72,11 @@ CREATE TABLE IF NOT EXISTS `projects` (
   `id_adviser` int(11) NOT NULL,
   `date_add` date NOT NULL,
   `note` float NOT NULL,
-  `investigation_line` varchar(200) COLLATE utf8_unicode_ci NOT NULL,
+  `id_investigation_line` int(11) NOT NULL,
   PRIMARY KEY (`id`),
-  KEY `id_adviser` (`id_adviser`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=1 ;
+  KEY `id_adviser` (`id_adviser`),
+  KEY `id_investigation_line` (`id_investigation_line`)
+) ENGINE=InnoDB  DEFAULT CHARSET=utf8 COLLATE=utf8_unicode_ci AUTO_INCREMENT=3 ;
 
 --
 -- Constraints for dumped tables
@@ -79,6 +92,7 @@ ALTER TABLE `author`
 -- Constraints for table `projects`
 --
 ALTER TABLE `projects`
+  ADD CONSTRAINT `projects_ibfk_2` FOREIGN KEY (`id_investigation_line`) REFERENCES `investigation_line` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
   ADD CONSTRAINT `projects_ibfk_1` FOREIGN KEY (`id_adviser`) REFERENCES `adviser` (`id`) ON DELETE CASCADE ON UPDATE CASCADE;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
